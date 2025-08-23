790c2907bc8d01a64be5896b35a9cfef0354dac8
Riyaz3777 <8019454867srz@gmail.com>
2025-08-23 19:50:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
-dotenv.config();
-connectDB();
-
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("User Management System API is running...");
-});
+// Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
