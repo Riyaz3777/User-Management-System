12968132e7187636a48a325700b9ce7cefb737d1
Riyaz3777 <8019454867srz@gmail.com>
2025-08-24 12:06:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const cors = require("cors"); // <--- CORS module
 
 require("dotenv").config();
 const app = express();
-app.use(express.json());
 
+// Middleware
+app.use(express.json());
+app.use(cors()); // <--- allow all origins
+
+// Connect to MongoDB
 connectDB();
 
 // Routes
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
